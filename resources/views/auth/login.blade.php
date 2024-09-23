0c6fc98a6543f442bfce53b9d427ed55564c2451
wapmgit <33326378+wapmgit@users.noreply.github.com>
2024-09-22 21:24:12 -0430

    act inicio app

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Acceso</div>
+                <div class="card-header">Acceso -> Version Lite</div>
 
                 <div class="card-body">
 					<div class="row mb-12">
