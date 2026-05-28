ee66997e9ddf00a78123edc7d5ce82f7382e2a52
wapmgit <33326378+wapmgit@users.noreply.github.com>
2026-05-28 18:16:12 -0430

    mostrar stock en edit

--- resources/views/almacen/articulo/edit.blade.php
+++ resources/views/almacen/articulo/edit.blade.php
@@ -2,7 +2,7 @@
 @section ('contenido')
 	<div class="row">
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<h3>Editar articulo: {{ $articulo->nombre}}</h3>
+			<h3>Editar articulo: {{ $articulo->nombre}} -> Stock {{$articulo->stock}}</h3>
 						@if (count($errors)>0)
 			<div class="alert alert-danger">
 				<ul>
